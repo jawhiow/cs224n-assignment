fcb82124dead6de3955ed9de55e817619c61e7a2
姜涛 <451241072@qq.com>
2018-10-28 09:54:33 +0800

    sigmoid问题解决

--- assignment1/q2_sigmoid.py
+++ assignment1/q2_sigmoid.py
@@ -15,7 +15,7 @@ def sigmoid(x):
     """
 
     ### YOUR CODE HERE
-    raise NotImplementedError
+    s = 1.0 / (1 + np.exp(-x))
     ### END YOUR CODE
 
     return s
@@ -35,7 +35,9 @@ def sigmoid_grad(s):
     """
 
     ### YOUR CODE HERE
-    raise NotImplementedError
+    # 需要注意拿到的数据是sigmod后的数据，不是原始的x，所以求导的时候需要注意:
+    # 把原始数据x转化成sigmoid(x)
+    ds = s * (1 - s)
     ### END YOUR CODE
 
     return ds
@@ -46,21 +48,23 @@ def test_sigmoid_basic():
     Some simple tests to get you started.
     Warning: these are not exhaustive.
     """
-    print ("Running basic tests...")
+    print("Running basic tests...")
     x = np.array([[1, 2], [-1, -2]])
     f = sigmoid(x)
     g = sigmoid_grad(f)
-    print (f)
+    print("sigmoid is: ")
+    print(f)
     f_ans = np.array([
         [0.73105858, 0.88079708],
         [0.26894142, 0.11920292]])
     assert np.allclose(f, f_ans, rtol=1e-05, atol=1e-06)
-    print (g)
+    print("gradient is :")
+    print(g)
     g_ans = np.array([
         [0.19661193, 0.10499359],
         [0.19661193, 0.10499359]])
     assert np.allclose(g, g_ans, rtol=1e-05, atol=1e-06)
-    print ("You should verify these results by hand!\n")
+    print("You should verify these results by hand!\n")
 
 
 def test_sigmoid():
@@ -70,7 +74,7 @@ def test_sigmoid():
     This function will not be called by the autograder, nor will
     your tests be graded.
     """
-    print ("Running your tests...")
+    print("Running your tests...")
     ### YOUR CODE HERE
     raise NotImplementedError
     ### END YOUR CODE
@@ -78,4 +82,4 @@ def test_sigmoid():
 
 if __name__ == "__main__":
     test_sigmoid_basic()
-    test_sigmoid()
+    # test_sigmoid()
